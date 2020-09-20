b84fd39dddcb3cb3fd25a0674bcab44f55a96c0a
GeorgeOfSystem <georgeilr99@gmail.com>
2020-09-20 18:32:17 -0400

    Home Nav

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './homeComponent/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule
   ],
   declarations: [HomeComponent]
 })
